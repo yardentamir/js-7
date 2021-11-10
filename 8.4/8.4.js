ff4d7713f501ce240f590320dd5e1d96123bef9b
yarentamir <yarden44441@gmail.com>
2021-11-10 14:18:14 +0200

    commit

---
+++ 8.4/8.4.js
@@ -0,0 +1,8 @@
+(function () {
+  "use strict";
+
+  const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
+  const countLetters = (arr) => {
+    for (let i = 0; i < arr.length; i++) {}
+  };
+})();
