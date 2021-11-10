29b2229a4f52dbe6b6b355041477a469fc906f41
yarentamir <yarden44441@gmail.com>
2021-11-10 12:57:29 +0200

    8.2

--- 8.2/8.2.js
+++ 8.2/8.2.js
@@ -1,3 +1,23 @@
 (function () {
   "use strict";
+
+  const myCountry = {
+    country: "Israel",
+    capital: "Jerusalem",
+    language: "Hebrew",
+    population: 700,
+    neighbors: ["Jordan", "Egypt", "Syria", "Lebanon"],
+    describe() {
+      console.log(`${this.country} has ${this.population} million people, their mother tongue is ${this.language}, they
+      have ${this.neighbors.length} neighboring countries and a capital called ${this.capital}`);
+    },
+    checkIsland() {
+      return this.neighbors.length <= 0
+        ? (this.isIsland = true)
+        : (this.isIsland = false);
+    },
+  };
+
+  myCountry.describe();
+  console.log(myCountry.checkIsland());
 })();
