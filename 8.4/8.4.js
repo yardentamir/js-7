2239511185d30120e4c8c561c5651df638f4c3d0
yarentamir <yarden44441@gmail.com>
2021-11-10 16:33:35 +0200

    8.4

--- 8.4/8.4.js
+++ 8.4/8.4.js
@@ -3,5 +3,21 @@
 
   const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
   const countLetters = (arr) => {
+    const myObj = {};
+    const connectedWords = arr.join("").replace(/\s/g, "").toLowerCase();
+    for (let i = 0; i < connectedWords.length; i++) {
+      let counter = 1;
 
+      for (let j = i + 1; j < connectedWords.length - 1; j++) {
+        if (connectedWords[i] === connectedWords[j]) counter++;
+      }
+
+      if (!myObj.hasOwnProperty(connectedWords[i])) {
+        myObj[connectedWords[i]] = counter;
+      }
+    }
+    return myObj;
+  };
+
+  console.log(countLetters(array));
 })();
